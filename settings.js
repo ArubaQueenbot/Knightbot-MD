244cffc8184c473c1c6a2b7c89b3892afab0d194
ArubaQueenbot <fatimakongoila@gmail.com>
2025-06-04 10:00:02 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Queen Bot",
+  botOwner: '☠꧁♕𝕽𝖊𝖇𝖊𝖑𝕼𝖚𝖊𝖊𝖓ⁱˢʰᵉʳ༒꧂ᒄᵃʳᵏ𓃠', // Your name
+  ownerNumber: 966550268506', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
